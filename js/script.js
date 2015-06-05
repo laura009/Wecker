7de522d0e5f1b12a1857c3d3aa7898d385148fb2
Laura Greco <laura.greco@me.com>
2015-06-05 11:03:34 +0200

    add geolocation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	$.ajax({
+	/*$.ajax({
 		dataType:'jsonp',
 		url:'https://api.forecast.io/forecast/f5d8630e1c9fdb9adf845910a7d5e4fd/47.379022,%208.541001?callback=?&units=si'
 			//API-Key sollte nicht hier stehen (Klau) -> Spezieller Dienst verwenden!
 			//Koordinaten
 			//?callback=?: JSON packt Code in eine Funktion und sendet diese (im JS darf man nicht Code von einem anderen Server laden)
 
-	}).done(function(data){
+	}).done(function(data){*/
 		
 		navigator.geolocation.getCurrentPosition(function(){
 			console.log(position);
