31ec3f7935fb446bbb4f835690fdf0aa7f8569d6
Laura Greco <laura.greco@me.com>
2015-06-05 11:16:02 +0200

    add geolocation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){		
 	navigator.geolocation.getCurrentPosition(function(position){
 		console.log(position);
-
 		$('.longitude').text(position.coords.longitude);
-
-		var $h1	= $('h1');
-		$h1.text(data.currently.apparentTemperature+ ' °C');
-
-		var $p = $('p');
-		$p.text(data.flags['metno-license']);	//wegen Divis: Punkt weglassen plus [''] 
+		$('.latitude').text(position.coords.latitude);
+		$('.accuracy').text(position.coords.accuracy);
 
 	});
 });
