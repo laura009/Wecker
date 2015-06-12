e9c36eefc82271b038eb87774d11fc109699c126
Laura Greco <laura.greco@me.com>
2015-06-12 09:01:54 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){		
 	navigator.geolocation.getCurrentPosition(function(position){
 		console.log(position);
-		
-		$('.longitude').text(position.coords.longitude);
-		$('.latitude').text(position.coords.latitude);
-		$('.accuracy').text(position.coords.accuracy);
 
-		https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE
+		var koordinaten = {
+			longitude: 	position.coords.longitude,
+			latitude: 	position.coords.latitude
+		};
+		
+		// Anzeige der Koordinaten
+		//$('.longitude').text(position.coords.longitude);
+		//$('.latitude').text(position.coords.latitude);
+		//$('.accuracy').text(position.coords.accuracy);
+
+
+		// API-Key: f5d8630e1c9fdb9adf845910a7d5e4fd
+		$.ajax({
+			url:  'https://api.forecast.io/forecast/f5d8630e1c9fdb9adf845910a7d5e4fd/' + koordinaten.latitude + ',' + koordinaten.longitude
+			data: {
+				units: 'si',
+				lang: 'de'
+			},
+			
+			// Damit wir mit JavaScript Daten von einem externen Webserver laden können
+			// JSONP packt Anfrage in eine Methode
+			dataType: 'jsonp'
+		}).done(function(data){
+			console.log(data);
+
+		});
+
+
+
 	});
 });
