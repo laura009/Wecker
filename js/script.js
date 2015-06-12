a81782e1977f378456b9be8e2638a04b02a920fb
Laura Greco <laura.greco@me.com>
2015-06-12 10:47:47 +0200

    add geocoding

--- js/script.js
+++ js/script.js
@@ -30,16 +30,17 @@ $(document).ready(function(){
 			$('.apparentTemperature').text(data.currently.apparentTemperature+ ' °C');
 
 			//Google Geocoding Anfrage
-			$.ajax:{
+			$.ajax:({
 				url:'https://maps.googleapis.com/maps/api/geocode/json',
 				data:{
 					latlng: koordinaten.latitude + ',' + koordinaten.longitude,
 					key: 'AIzaSyDgYh-UffzCV54XCcReML4WSqyb0_zv8x8',
 					language: 'de'
-				}
-			}
+				},
+			}).done(function(data){
+				console.log(data);
+			});
 		});
-
 	});
 });
 
