baf26f0fc9035b984594df707801f4f16455a0d5
Laura Greco <laura.greco@me.com>
2015-07-01 22:14:54 +0200

    google maps

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){	
 
 	var skycons = new Skycons({
-			color: "#fff",
-			resizeClear:true
+		color: "#fff",
+		resizeClear:true
 	});
 
-	navigator.geolocation.getCurrentPosition(function(position){
+	navigator.geolocation.getCurrentPosition(function(position) {
 		// console.log(position);
 
 		var koordinaten; = {
@@ -16,8 +16,8 @@ $(document).ready(function(){
 		// Forcast
 		// API-Key: f5d8630e1c9fdb9adf845910a7d5e4fd
 		$.ajax({
-			url:'https://api.forecast.io/forecast/f5d8630e1c9fdb9adf845910a7d5e4fd/' + koordinaten.latitude + ',' + koordinaten.longitude,
-			data:{
+			url: 'https://api.forecast.io/forecast/a955df0e9afe8c822ebb3adf30265fb6/' + koordinaten.latitude + ',' + koordinaten.longitude,
+			data: {
 				units: 'si',
 				lang: 'de'
 			},
@@ -67,13 +67,13 @@ $(document).ready(function(){
 			// mein Google API-Code: AIzaSyAcTuBQBKhiEXs3S4TZ0Pr1EzdqCSCMxig
 			// Crossrequest erlaubt
 			$.ajax({
-				url:'https://maps.googleapis.com/maps/api/geocode/json',
-				data:{
+				url: 'https://maps.googleapis.com/maps/api/geocode/json',
+				data: {
 					latlng: koordinaten.latitude + ',' + koordinaten.longitude,
 					key: 'AIzaSyDgYh-UffzCV54XCcReML4WSqyb0_zv8x8',
 					language: 'de'
 				}
-			}).done(function(data){
+			}).done(function(data) {
 				// console.log(data);
 
 				// Anzeige der Adresse
