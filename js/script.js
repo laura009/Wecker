4f14cd7260a02b773350856decf2e2eadfe28d0c
Laura Greco <laura.greco@me.com>
2015-07-01 22:06:23 +0200

    google maps

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
 		// console.log(position);
 
 		var koordinaten; = {
-			longitude: 	 position.coords.longitude,
-			latitude: 	 position.coords.latitude
-		};		
+			longitude: 	position.coords.longitude,
+			latitude: 	position.coords.latitude
+		};
 			
 		// Forcast
 		// API-Key: f5d8630e1c9fdb9adf845910a7d5e4fd
